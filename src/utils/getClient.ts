7744d6a5fdc84a8bce0b592ae00c1435394449ab
harshilp25 <harshildpatel255@gmail.com>
2024-08-10 07:32:07 +0530

    " postgre_uri final changed "

--- src/utils/getClient.ts
+++ src/utils/getClient.ts
@@ -1,9 +1,7 @@
 import { Client } from "pg";
 
 export const getClient = async () => {
-  const client = new Client(
-    "postgresql://postgre_test_owner:uQSf4ixI7pec@ep-weathered-field-a7029hbe.ap-southeast-2.aws.neon.tech/postgre_test?sslmode=require"
-  );
+  const client = new Client(process.env.postgre_uri);
 
   client
     .connect()
